d11c5e69db071e8af8fb27f61e8f41f51cca9236
Etc <diamondguy9700@gmail.com>
2021-05-02 22:20:44 -0600

    Fixed hexxuit.js

--- hexxuit.js
+++ hexxuit.js
@@ -1,6 +1,6 @@
 // A CLI version of the sspace tool //
 
-const f = require('./hexxpace');
+const f = require('./hexx');
 
 const readline = require('readline').createInterface({
         input: process.stdin,
